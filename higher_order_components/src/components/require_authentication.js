10fb21c4f791a038fdfdf0651dc05bf2c5358776
Malachi Gray <graymd.cfi@gmail.com>
2017-03-09 17:54:13 -0500

    nesting higher order components 42 complete

---
+++ higher_order_components/src/components/require_authentication.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+export default function(ComposedComponent) {
+  class Authentication extends Component {
+    static contextTypes = {
+      router: React.PropTypes.object
+    }
+    render() {
+      console.log(this.context);
+      return <ComposedComponent {...this.props} />
+    }
+  }
+
+function mapStateToProps(state) {
+  return { authenticated: state.authenticated };
+}
+
+  return connect(mapStateToProps)(Authentication);
+}
