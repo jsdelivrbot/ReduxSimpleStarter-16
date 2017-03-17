57d0c8b51d53616d3054a4e9d953c4e2b6e6e011
Malachi Gray <graymd.cfi@gmail.com>
2017-03-16 21:41:22 -0400

    complete async middleware

--- middlewares2/src/middlewares/async.js
+++ middlewares2/src/middlewares/async.js
@@ -1,7 +1,17 @@
 export default function({ dispatch }) {
   return next => action => {
-    console.log(action);
+    // if action does not have a payload or the payload does not have a .then property we don't care about it, send it on
+    if (!action.payload || !action.payload.then) {
+      return next(action);
+    }
+    console.log('We have a promise', action);
 
-    next(action);
+    //Make sure the action's promise resolves
+    action.payload
+      .then((response) => {
+        // create a new action with the old type, but replace the promise with the response data
+        const newAction = { ...action, payload: response }
+        dispatch(newAction);
+      });
   };
 }
