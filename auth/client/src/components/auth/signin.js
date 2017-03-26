9203d298f4bd48ef60437e648f7c9b4f4f1bab61
Malachi Gray <graymd.cfi@gmail.com>
2017-03-26 13:34:46 -0400

    add action creator and downgrade redux form to 4.2

---
+++ auth/client/src/components/auth/signin.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { reduxForm } from 'redux-form';
+
+class Signin extends Component {
+  constructor() {
+    super();
+    this.handleFormSubmit = this.handleFormSubmit.bind(this);
+  }
+  handleFormSubmit({ email, password }) {
+    console.log(email, password);
+    // Need to do something to log user in
+  }
+
+  render() {
+    const { handleSubmit, fields: { email, password }} = this.props;
+    return (
+      <form onSubmit={handleSubmit(this.handleFormSubmit)}>
+        <fieldset className='form-group'>
+          <label>Email:</label>
+          <input {...email} className='form-control'/>
+        </fieldset>
+        <fieldset className='form-group'>
+          <label>Password:</label>
+          <input {...password} className='form-control'/>
+        </fieldset>
+        <button action='submit' className='btn btn-primary'>Sign In</button>
+      </form>
+    );
+  }
+}
+
+export default reduxForm({
+  form: 'signin',
+  fields: ['email', 'password']
+})(Signin);
