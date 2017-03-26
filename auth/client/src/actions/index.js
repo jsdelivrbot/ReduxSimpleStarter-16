6de973468ad89af6f3f94cb925a427b4eb6f31f8
Malachi Gray <graymd.cfi@gmail.com>
2017-03-26 13:48:50 -0400

    signin action creator

--- auth/client/src/actions/index.js
+++ auth/client/src/actions/index.js
@@ -1,11 +1,15 @@
+import axios from 'axios';
 
+const ROOT_URL = 'http://localhost:3090';
 
 export function signinUser({ email, password }) {
   //redux thunk gives us access to dispatch
+  // can dispatch action at anytime we want - doesn't have to happen immediately
+
   return function(dispatch) {
 
     // Submit email/password to the server
-
+    axios.post(`${ROOT_URL}/signin`, { email, password })
     // If request is good...
     // - Update state to indicate user is authenticated
     // - Save the JWT Token
--- auth/client/src/components/auth/signin.js
+++ auth/client/src/components/auth/signin.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { reduxForm } from 'redux-form';
+import * as actions from '../../actions'
 
 class Signin extends Component {
   constructor() {
@@ -9,6 +10,7 @@ class Signin extends Component {
   handleFormSubmit({ email, password }) {
     console.log(email, password);
     // Need to do something to log user in
+    this.props.signinUser({ email, password });
   }
 
   render() {
@@ -32,4 +34,4 @@ class Signin extends Component {
 export default reduxForm({
   form: 'signin',
   fields: ['email', 'password']
-})(Signin);
+}, null, actions)(Signin);
