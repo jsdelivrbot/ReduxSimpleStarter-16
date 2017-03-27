81e5fb09060a66188bddca30ad909b1665fc6aed
Malachi Gray <graymd.cfi@gmail.com>
2017-03-26 21:07:39 -0400

    updating auth state

--- auth/client/src/actions/index.js
+++ auth/client/src/actions/index.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { browserHistory } from 'react-router';
+import { AUTH_USER } from './types';
 
 const ROOT_URL = 'http://localhost:3090';
 
@@ -13,7 +14,9 @@ export function signinUser({ email, password }) {
     axios.post(`${ROOT_URL}/signin`, { email, password })
       .then(response => {
         // If request is good...
+
         // - Update state to indicate user is authenticated
+        dispatch({ type: AUTH_USER });
         // - Save the JWT Token
         // - redirect to the route '/feature'
         browserHistory.push('/feature');
