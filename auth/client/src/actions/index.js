af9ac38eabdd6ad1bd58842e5df19a0f102723fe
Malachi Gray <graymd.cfi@gmail.com>
2017-03-26 21:01:17 -0400

    programmatic navigation on successful signin

--- auth/client/src/actions/index.js
+++ auth/client/src/actions/index.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { browserHistory } from 'react-router';
 
 const ROOT_URL = 'http://localhost:3090';
 
@@ -10,13 +11,20 @@ export function signinUser({ email, password }) {
 
     // Submit email/password to the server
     axios.post(`${ROOT_URL}/signin`, { email, password })
-    // If request is good...
-    // - Update state to indicate user is authenticated
-    // - Save the JWT Token
-    // - redirect to the route '/feature'
+      .then(response => {
+        // If request is good...
+        // - Update state to indicate user is authenticated
+        // - Save the JWT Token
+        // - redirect to the route '/feature'
+        browserHistory.push('/feature');
+      })
+      .catch(() => {
+        // If request is bad...
+        // - Show an error to the user
+      });
 
-    // If request is bad...
-    // - Show an error to the user
+
+
 
   }
 }
