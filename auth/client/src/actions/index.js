e62cbd24c4af07debe5c27d68108b1318c8391ab
Malachi Gray <graymd.cfi@gmail.com>
2017-03-26 21:18:19 -0400

    store jwt to local storage

--- auth/client/src/actions/index.js
+++ auth/client/src/actions/index.js
@@ -6,6 +6,7 @@ const ROOT_URL = 'http://localhost:3090';
 
 export function signinUser({ email, password }) {
   //redux thunk gives us access to dispatch
+  // thunk will call the returned function with dispatch
   // can dispatch action at anytime we want - doesn't have to happen immediately
 
   return function(dispatch) {
@@ -18,6 +19,7 @@ export function signinUser({ email, password }) {
         // - Update state to indicate user is authenticated
         dispatch({ type: AUTH_USER });
         // - Save the JWT Token
+        localStorage.setItem('token', response.data.token);
         // - redirect to the route '/feature'
         browserHistory.push('/feature');
       })
