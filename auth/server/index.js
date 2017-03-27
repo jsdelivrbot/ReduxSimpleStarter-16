abb682fe08861778fcf95dcbf07ecd7a5548bcc5
Malachi Gray <graymd.cfi@gmail.com>
2017-03-26 20:55:17 -0400

    add cors server side

--- auth/server/index.js
+++ auth/server/index.js
@@ -6,6 +6,7 @@ const morgan = require('morgan');
 const app = express();
 const router = require('./router');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 // DB setup
 mongoose.connect('mongodb://localhost:auth/auth');
@@ -14,6 +15,7 @@ mongoose.connect('mongodb://localhost:auth/auth');
 // App Setup
 //morgan is logging framework
 app.use(morgan('combined'));
+app.use(cors());
 app.use(bodyParser.json({ type: '*/*' }));
 router(app);
 
