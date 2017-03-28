59d12059937153d9799128d23610b117a1222793
Malachi Gray <graymd.cfi@gmail.com>
2017-03-28 17:42:05 -0400

    signout action creator

--- auth/client/src/actions/index.js
+++ auth/client/src/actions/index.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import { browserHistory } from 'react-router';
 import {
   AUTH_USER,
+  UNAUTH_USER,
   AUTH_ERROR } from './types';
 
 const ROOT_URL = 'http://localhost:3090';
@@ -38,4 +39,9 @@ export function authError(error) {
     type: AUTH_ERROR,
     payload: error
   }
+}
+
+export function signoutUser() {
+  localStorage.removeItem('token');
+  return { type: UNAUTH_USER }
 }
